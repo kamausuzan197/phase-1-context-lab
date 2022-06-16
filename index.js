f6558b1f51427472cf85a513c41afa4a1e10befe
kamausuzan197 <kamaususan197@gmail.com>
2022-06-16 21:39:22 +0300

    test

--- index.js
+++ index.js
@@ -9,15 +9,73 @@
  for you to use if you need it!
  */
 
-const allWagesFor = function () {
-    const eligibleDates = this.timeInEvents.map(function (e) {
-        return e.date
-    })
-
-    const payable = eligibleDates.reduce(function (memo, d) {
-        return memo + wagesEarnedOnDate.call(this, d)
-    }.bind(this), 0) // <== Hm, why did we need to add bind() there? We'll discuss soon!
-
-    return payable
+ function createEmployeeRecord([string1, string2, string3, number]) {
+    return {
+        firstName: string1,
+        familyName: string2,
+        title: string3,
+        payPerHour: number,
+        timeInEvents: [],
+        timeOutEvents: []
+    }
 }
 
+function createEmployeeRecords(array) {
+    return array.map(x => createEmployeeRecord(x))
+}
+
+function createTimeInEvent(dateStamp) {
+    const hour = dateStamp.split(' ')[1]
+    const date = dateStamp.split(' ')[0]
+    const timeInObj = {
+        type: 'TimeIn',
+        hour: parseInt(hour, 10),
+        date: date
+    }
+    //return employee record
+    const timeInEvents = this.timeInEvents
+    timeInEvents.push(timeInObj)
+    return this
+}
+
+function createTimeOutEvent (dateStamp) {
+    const hour = dateStamp.split(' ')[1]
+    const date = dateStamp.split(' ')[0]
+    const timeOutObj = {
+        type: 'TimeOut',
+        hour: parseInt(hour, 10),
+        date: date
+    }
+    //return employee record
+    const timeOutEvents = this.timeOutEvents
+    timeOutEvents.push(timeOutObj)
+    return this
+}
+
+function hoursWorkedOnDate (dateStamp) {
+    let timeIn = ''
+    let timeOut = ''
+    //find timeIn events that match date stamp
+    this.timeInEvents.forEach((x) => {
+        if (x.date === dateStamp) {
+            timeIn = x.hour
+        }
+    })
+    //find timeOut events that match date stamp
+    this.timeOutEvents.forEach((x) => {
+        if (x.date === dateStamp) {
+            timeOut = x.hour
+        }
+    })
+    //calculate hours worked 
+    const hoursWorked = (timeOut - timeIn)/100
+    //return hours worked as integer
+    return hoursWorked
+
+}
+
+function wagesEarnedOnDate (dateStamp) {
+    const hoursWorked = hoursWorkedOnDate.call(this, dateStamp)
+    const wagesEarned = hoursWorked * this.payPerHour
+    return wagesEarned
+}
